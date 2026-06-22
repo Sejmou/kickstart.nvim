572dcedd53f09424560f0172d693f0b559cc1df3
Sejmou <samo.kolter@gmail.com>
2026-06-22 18:01:25 +0200

    macOS tweaks logout prompt (necessary for keyboard settings to apply apparently)

--- apply-macos-ux-tweaks.sh
+++ apply-macos-ux-tweaks.sh
@@ -1,8 +1,13 @@
-# make typing faster and less annoying without having to get into settings UI
-defaults write -g KeyRepeat -int 2 # == Key repeat rate: Fast
-defaults write -g InitialKeyRepeat -int 15 # == Delay until repeat: Short
-
 # make Dock hide automatically and show/hide quicker than frustratingly slow default
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock autohide-delay -float 0 # disable delay
 defaults write com.apple.dock autohide-time-modifier -float 0.4 # speed up slide animation itself
+
+# make typing faster and less annoying without having to get into settings UI
+defaults write -g KeyRepeat -int 2 # == Key repeat rate: Fast
+defaults write -g InitialKeyRepeat -int 15 # == Delay until repeat: Short
+
+read -r -p "Applying keyboard settings requires logging out. Do it now? [y/N] " confirm
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+  osascript -e 'tell application "System Events" to log out'
+fi
